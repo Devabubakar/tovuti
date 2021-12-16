46a749f139d424ce378badec55ab19909af73530
Devabubakar <abubakarali3w@gmail.com>
2021-12-16 18:28:03 +0300

    bug:fix netlify

--- client/src/components/hero.jsx
+++ client/src/components/hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
 
-import { graphql } from 'gatsby';
+
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import Typography from '@mui/material/Typography';
 
